6a4bceede562ccf790b2cf185578d106ee3f0ac0
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-07-19 00:18:15 -0400

    18.1.8

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const mongoose = require('mongoose');
-
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,13 +7,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+  useUnifiedTopology: true 
+}); 
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
